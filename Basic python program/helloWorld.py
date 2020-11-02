17b80f2e292609bb67fba65e26553dfde3261ea1
VA09170 <63238555+VA09170@users.noreply.github.com>
2020-11-03 04:02:52 +1100

    hello world python program
    hello world uisng print and functions

---
+++ Basic python program/helloWorld.py
@@ -0,0 +1,11 @@
+# the following code will print hello world
+
+# one method
+print("hello world from the first method")
+
+# second method using function
+def display():
+    return "hello world from the function"
+# calling the function and storing the return value in the variable
+helloworld = display()
+print(helloworld)
